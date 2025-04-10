179786274286aee1554038635699a9924d582660
Ayush Mishra <ayush.mishra@digivive.com>
2025-04-10 14:12:08 +0530

    implement new API

---
+++ Backend/backend/database/migrations/2025_04_10_082554_create_driver_details_uploads_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up()
+{
+    Schema::create('driver_details_uploads', function (Blueprint $table) {
+        $table->id();
+        $table->string('photo')->nullable();
+        $table->string('education');
+        $table->integer('age');
+        $table->string('exact_location');
+        $table->string('pincode');
+        $table->string('zone');
+        $table->integer('driving_experience');
+        $table->integer('car_driving_experience');
+        $table->string('driving_licence_front')->nullable();
+        $table->string('driving_licence_back')->nullable();
+        $table->string('type_of_driving_licence');
+        $table->string('aadhar_card_front')->nullable();
+        $table->string('aadhar_card_back')->nullable();
+        $table->string('passbook_front')->nullable();
+        $table->string('account_number');
+        $table->string('bank_name');
+        $table->string('ifsc_code');
+        $table->string('account_holder_name');
+        $table->timestamps();
+    });
+}
+
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('driver_details_uploads');
+    }
+};
