53b58f6966951e753e82937906aea17e0e3923b7
Priya Kumari Singh <priyasingh2018jsr@gmail.com>
2025-05-05 22:12:52 +0530

    changes in insta,facebook,twitter images

--- Frontend/src/components/Footer.jsx
+++ Frontend/src/components/Footer.jsx
@@ -14,9 +14,9 @@ function Footer() {
         <div className="footer-section">
           <h2>Follow Us</h2>
           <div className="social-links">
-              <a href="https://www.facebook.com/share/196oNQ1qkF/"  target="_blank" rel="noopener noreferrer"> <img src="/facebook.jpg" alt="Facebook" /></a>
-              <a href="https://twitter.com/YourHandle" target="_blank" rel="noopener noreferrer" > <img src="/twitter.jpg" alt="Twitter" /></a>
-              <a href="https://www.instagram.com/sahyogforce?igsh=cjY2bzQ3MzA0ZTI1"  target="_blank"  rel="noopener noreferrer" > <img src="/instagram.jpg" alt="Instagram" /></a>
+              <a href="https://www.facebook.com/share/196oNQ1qkF/"  target="_blank" rel="noopener noreferrer"> <img src="facebook.jpg" alt="Facebook" /></a>
+              <a href="https://twitter.com/YourHandle" target="_blank" rel="noopener noreferrer" > <img src="twitter.jpg" alt="Twitter" /></a>
+              <a href="https://www.instagram.com/sahyogforce?igsh=cjY2bzQ3MzA0ZTI1"  target="_blank"  rel="noopener noreferrer" > <img src="instagram.jpg" alt="Instagram" /></a>
           </div>
           <p> contact@sahyogforce.com</p>
           <p> support@sahyogforce.com</p>
